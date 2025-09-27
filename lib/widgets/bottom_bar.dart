c89d4b243300949580bf1e06effcb3f5fc240333
ynz1010 <61936696+ynz1010@users.noreply.github.com>
2025-09-27 11:46:39 +0100

    Update bottom_bar.dart

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -1,11 +1,12 @@
-import 'package:Mirarr/widgets/search_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:Mirarr/moviesPage/mainPage.dart';
 import 'package:Mirarr/seriesPage/seriesPage.dart';
+import 'package:Mirarr/widgets/search_screen.dart';
+import 'package:Mirarr/widgets/shelf_page.dart';
 import 'package:Mirarr/widgets/login.dart';
 import 'package:Mirarr/widgets/profile.dart';
 import 'package:hive/hive.dart';
-import 'package:Mirarr/widgets/shelf_page.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
@@ -14,123 +15,89 @@ class BottomBar extends StatefulWidget {
   _BottomBarState createState() => _BottomBarState();
 }
 
-int _selectedIndex = 0;
-
 class _BottomBarState extends State<BottomBar> {
-  void toSeries() {
-    setState(() {
-      _selectedIndex = 1;
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const SerieSearchScreen()),
-      );
-    });
-  }
+  int _selectedIndex = 0;
 
   void toMovies() {
-    setState(() {
-      _selectedIndex = 0;
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const MovieSearchScreen()),
-      );
-    });
+    Navigator.push(context, MaterialPageRoute(builder: (_) => const MovieSearchScreen()));
   }
 
-  void toShelf() {
-    setState(() {
-      _selectedIndex = 3;
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => ShelfPage()),
-      );
-    });
+  void toSeries() {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => const SerieSearchScreen()));
   }
 
   void toSearch() {
-    setState(() {
-      _selectedIndex = 2;
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const SearchScreen()),
-      );
-    });
+    Navigator.push(context, MaterialPageRoute(builder: (_) => const SearchScreen()));
+  }
+
+  void toShelf() {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => ShelfPage()));
   }
 
   void toAccount() async {
     final box = await Hive.openBox('sessionBox');
     final sessionData = box.get('sessionData');
-    setState(() {
-      _selectedIndex = 4;
-      if (sessionData != null) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ProfilePage()),
-        );
-      } else {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const LoginPage()),
-        );
+    if (sessionData != null) {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => ProfilePage()));
+    } else {
+      Navigator.push(context, MaterialPageRoute(builder: (_) => const LoginPage()));
+    }
+  }
+
+  void _handleRemoteNavigation(RawKeyEvent event) {
+    if (event is RawKeyDownEvent) {
+      if (event.logicalKey == LogicalKeyboardKey.arrowLeft) {
+        setState(() {
+          _selectedIndex = (_selectedIndex - 1 + 5) % 5;
+        });
+      } else if (event.logicalKey == LogicalKeyboardKey.arrowRight) {
+        setState(() {
+          _selectedIndex = (_selectedIndex + 1) % 5;
+        });
+      } else if (event.logicalKey == LogicalKeyboardKey.select) {
+        switch (_selectedIndex) {
+          case 0: toMovies(); break;
+          case 1: toSeries(); break;
+          case 2: toSearch(); break;
+          case 3: toShelf(); break;
+          case 4: toAccount(); break;
+        }
       }
-    });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      selectedItemColor: Theme.of(context).highlightColor,
-      selectedIconTheme: IconThemeData(color: Theme.of(context).highlightColor),
-      selectedFontSize: 16,
-      unselectedItemColor: Theme.of(context).primaryColor,
-      currentIndex: _selectedIndex,
-      onTap: (int index) {
-        if (_selectedIndex != index) {
-          if (index == 0) {
-            toMovies();
-          } else if (index == 1) {
-            toSeries();
-          } else if (index == 4) {
-            toAccount();
-          } else if (index == 3) {
-            toShelf();
-          } else if (index == 2) {
-            toSearch();
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: _handleRemoteNavigation,
+      child: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).highlightColor,
+        selectedIconTheme: IconThemeData(color: Theme.of(context).highlightColor),
+        selectedFontSize: 16,
+        unselectedItemColor: Theme.of(context).primaryColor,
+        currentIndex: _selectedIndex,
+        onTap: (int index) {
+          if (_selectedIndex != index) {
+            setState(() => _selectedIndex = index);
+            switch (index) {
+              case 0: toMovies(); break;
+              case 1: toSeries(); break;
+              case 2: toSearch(); break;
+              case 3: toShelf(); break;
+              case 4: toAccount(); break;
+            }
           }
-        }
-      },
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.movie,
-          ),
-          label: 'Movies',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.local_movies,
-          ),
-          label: 'Series',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.search,
-          ),
-          label: 'Search',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.shelves,
-          ),
-          label: 'Shelf',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.person,
-          ),
-          label: 'Account',
-        ),
-      ],
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.movie), label: 'Movies'),
+          BottomNavigationBarItem(icon: Icon(Icons.local_movies), label: 'Series'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.shelves), label: 'Shelf'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
+        ],
+      ),
     );
   }
 }
